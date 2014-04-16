98029d9dfad418bbdc8cb50dc3b50bf870a48238
Drew Moore <teloaster@yahoo.com>
2014-04-16 13:07:23 -0500

    Cheers exercise completed.

---
+++ cheers.rb
@@ -0,0 +1,21 @@
+puts "What's Your Name, Yo?"
+
+name = gets.chomp
+vowels = ["a", "e", "f", "i", "o", "h", "l", "m", "n", "r", "s", "x", "A", "E", "F", "I", "O", "H", "L", "R", "S", "M", "N", "X"]
+
+name.each_char do |character|
+  article = "a"
+  if vowels.include? character
+    article = "an"
+  end
+  puts "Give me " + article + " " + character.capitalize
+end
+
+apostrophe = "'s"
+
+if name[-1] == ("s" || "S")
+  apostrophe = "'"
+end
+
+puts name + apostrophe + " " + "just Grand!"
+
